eb08a3233fc4e7b0b96228be446558c432990138
GGGin <fm369o802340@163.com>
2016-01-13 16:46:05 +0800

    baucis generate restful api just this!

---
+++ src/server.js
@@ -0,0 +1,48 @@
+// __Dependencies__
+
+var express = require('express');
+var mongoose = require('mongoose');
+var baucis = require('baucis');
+var swagger = require('baucis-swagger');
+var swagger2 = require('baucis-swagger2');
+
+// __Main Program__
+
+// Connect to the Mongo instance
+mongoose.connect('mongodb://gggin:777888@:27017/two');
+
+// Create a Mongoose schema
+var Vegetable = new mongoose.Schema({ name: String });
+
+// Note that Mongoose middleware will be executed as usual
+Vegetable.pre('save', function (next) {
+    console.log('A vegetable was saved to Mongo: %s.', this.get('name'));
+    next();
+});
+
+// Register the schema
+mongoose.model('vegetable', Vegetable);
+
+// Create dummy data
+var names = [ 'tomato', 'turnip', 'lovage', 'snap pea', 'carrot', 'zucchini' ];
+var vegetables = names.map(function (name) { return { name: name } });
+
+// Clear the database of old vegetables
+mongoose.model('vegetable').remove(function (error) {
+    if (error) throw error;
+
+    // Put the fresh vegetables in the database
+    mongoose.model('vegetable').create(vegetables, function (error) {
+        if (error) throw error;
+
+        // Create the API routes
+        baucis.rest('vegetable');
+
+        // Create the app and listen for API requests
+        var app = express();
+        app.use('/api', baucis());
+        app.listen(3333);
+
+        console.log('Server listening on port 3333.');
+    });
+});
